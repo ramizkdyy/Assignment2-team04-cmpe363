6ecbab3f89598350c52ea21d7c4637cb6c876079
ramizkdyy <ramiz.kadayifci@bilgiedu.net>
2022-12-29 22:24:15 +0300

    Proje dosyası ekle.

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 273px;
+        }
+    </style>
+</head>
+<body style="height: 288px">
+    <form id="form1" runat="server">
+        <div>
+            Show infos of employee<br />
+            <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Show" Width="48px" />
+                <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            </div>
+                </div>
+             
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Assignment2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+       
+
+        private SqlConnection GetConnection()
+        {
+            
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
+            connection.Open();
+            return connection;
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM tblEmployee", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    Label1.Text += reader.GetString(reader.GetOrdinal("EmpName")) + " " + reader.GetString(reader.GetOrdinal("EmpSurname")) + " " + reader.GetString(reader.GetOrdinal("EmpAddress")) + " " + reader.GetString(reader.GetOrdinal("EmpPhone")) + "<br>";
+                   
+                }
+             
+                reader.Close();
+                connection.Close();
+            }
+            
+        }
+
+    }
+}
